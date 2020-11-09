9308fb49a1dd6d11d59bcfc0e6551a8f46aaeab4
Antoine <sirpa.antoine@gmail.com>
2020-11-09 20:47:52 +0100

    creation question controller

---
+++ src/Controller/QuestionController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class QuestionController
+{
+    
+    /**
+     * 
+     */
+    public function __construct()
+    {
+        
+    }
+    
+    /**
+     * 
+     */
+    public function homepage()
+    {
+        return new Response('what a bewitching controller we have conjured');
+    }
+}
